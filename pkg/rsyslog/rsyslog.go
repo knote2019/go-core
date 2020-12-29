5ff34aaa1e8b2e0f46c7e98599608890cba683a9
emigkag <kenny.kang@ericsson.com>
2020-12-29 21:19:37 +0800

    add rsyslog note

---
+++ pkg/rsyslog/rsyslog.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"log"
+	"log/syslog"
+)
+
+// TODO: fix bug
+func main() {
+	// 如果network是空，Dial会连接到本地的syslog服务器。
+	sysLog, err := syslog.Dial("", "",syslog.LOG_ERR, "Saturday")
+	if err != nil {
+		log.Fatal(err)
+	}
+	sysLog.Emerg("Hello world!")
+}
